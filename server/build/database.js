1b53e429d2c72ce4161701107a04d63c3f0f109e
Juan Carlos Cadavid Ruiz <jucacar28@hotmail.com>
2019-08-09 18:32:34 -0500

    JC-FRONT

--- server/build/database.js
+++ server/build/database.js
@@ -3,10 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const msnodesqlv8_1 = require("mssql/msnodesqlv8");
+const mssql_1 = require("mssql");
 const keys_1 = __importDefault(require("./keys"));
 var connections = keys_1.default.database;
-const pool = new msnodesqlv8_1.ConnectionPool(connections);
+const pool = new mssql_1.ConnectionPool(connections);
 pool.connect().then(connection => {
     pool.on('error', res => {
     });
